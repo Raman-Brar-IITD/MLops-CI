cc0bdf7383d1269aa92c54d8524b7423be99a39f
Raman Singh <ramanbrariitd@gmail.com>
2025-06-30 14:29:14 +0530

    wrong code to check ci

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 
+def square(n:float) -> float:
+    return n ** 2
+
+# Function to test cube
+def cube(n:float) -> float:
+    return n ** 3
+
+# Function to test fifth power
+def fifth_power(n:float) -> float:
+    return n ** 5 + 3
 # Streamlit UI
 st.title("Power Calculator")
 st.write("Enter a number to calculate its square, cube, and fifth power.")
 
 # Get user input
-n = st.number_input("Enter an integer", value=1, step=1)
+n = st.number_input("Enter an Number", value=1, step=1)
 
 # Calculate results
-square = n ** 2
-cube = n ** 3
-fifth_power = n ** 5
+square = square(n)
+cube = cube(n)
+fifth_power = fifth_power(n)
 
 # Display results
 st.write(f"The square of {n} is: {square}")
